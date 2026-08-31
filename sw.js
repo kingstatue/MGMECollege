45b7f9051fe384a1757eccbdecf6e7a10aabb906
kingstatue <kingstatue@gmail.com>
2026-08-31 23:58:59 +0530

    section prefix above

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const CACHE_PREFIX = 'mgmec-absentee-informer';
-const CACHE_NAME = 'mgmec-absentee-informer-v115-bulk-generator';
+const CACHE_NAME = 'mgmec-absentee-informer-v116-prefix-above';
 
 function isOwnCache(name) {
   return String(name || '').indexOf(CACHE_PREFIX) === 0;
