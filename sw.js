24418c86cf5493b3de32af2a37a14f7cbd7d7c59
kingstatue <kingstatue@gmail.com>
2026-09-14 15:14:09 +0530

    parent informer UI

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'mgmec-absentee-informer-v137-sheet-sync';
+const CACHE_NAME = 'mgmec-absentee-informer-v139-hod-workspace';
 
 // Install Event - Instant non-blocking activation (0ms SW installation)
 self.addEventListener('install', (event) => {
